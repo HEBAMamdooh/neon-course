8049b017c587267d6357bf06b0e32e94c3a7e0cf
Heba Mamdooh <52792349+HebaMamdooh@users.noreply.github.com>
2022-10-04 21:04:44 +0200

    compare my users with teacher
    still not working

---
+++ resources/views/admin/users.blade.php
@@ -0,0 +1,52 @@
+@extends ('admin.admin-app');
+@section('content')
+<h2> Welcome : {{ Auth::user()->name }}</h2>
+<span> the user number {{ Auth::user()->id }} of </span>
+<hr>
+<table class="table card-table table-responsive table-responsive-large" style="width:100%">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>User Name</th>
+      <th class="d-none d-md-table-cell">Email </th>
+      <th class="d-none d-md-table-cell">Status</th>
+      <th class="d-none d-md-table-cell">Created at</th>
+      <th>Status</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    @php($i=1)
+    @foreach ($users as $user)
+
+    <tr>
+      <td>{{ $i++ }}</td>
+      <td>
+        <a class="text-dark" href=""> {{ $user->name }}</a>
+      </td>
+      <td class="d-none d-md-table-cell">{{ $user->email }}</td>
+      <td class="d-none d-md-table-cell">{{ $user->email_verified_at }}</td>
+      <td class="d-none d-md-table-cell">$230</td>
+      <td>
+        <span class="badge badge-success">{{ $user->created_at->diffForHumans() }}</span>
+      </td>
+      <td class="text-right">
+        <div class="dropdown show d-inline-block widget-dropdown">
+          <a class="dropdown-toggle icon-burger-mini" href="" role="button" id="dropdown-recent-order1"
+            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-display="static"></a>
+          <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown-recent-order1">
+            <li class="dropdown-item">
+              <a href="#">View</a>
+            </li>
+            <li class="dropdown-item">
+              <a href="#">Remove</a>
+            </li>
+          </ul>
+        </div>
+      </td>
+    </tr>
+
+    @endforeach
+  </tbody>
+</table>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 use App\http\Controller\backendController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Models\User;
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 Route::get('/logout', [backendController::class, 'logout'])->name('logout');
 
 Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
+  'auth:sanctum',
+  config('jetstream.auth_session'),
+  'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('admin.index');
-    })->name('dashboard');
+  Route::get('/dashboard', function () {
+    return view('admin.index');
+  })->name('dashboard');
+
+  Route::get('/users', function () {
+    $users = User::all();
+    return view('admin.users');
+  })->name('users');
 });
